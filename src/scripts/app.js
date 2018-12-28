04bb53f4299fd67dd3099b9c71747042dedb3dee
CJ Reyes <itscjreyes@gmail.com>
2018-12-28 15:26:41 -0500

    Homepage for desktop complete

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,25 @@
-const hello = 'World';
-console.log(hello);
+const app = {};
+
+app.init = function(){
+    app.slider();
+};
+
+app.slider = function(){
+    $('.fullSlider').slick({
+        centerMode: true,
+        slidesToShow: 3,
+        dots: true,
+        prevArrow: '<a class="slick-prev slick-arrow"><i class="fa fa-angle-left"></i></a>',
+        nextArrow: '<a class="slick-next slick-arrow"><i class="fa fa-angle-right"></i></a>'
+    });
+
+    $('.testimonialSlider').slick({
+        prevArrow: '<a class="slick-prev slick-arrow"><i class="fa fa-angle-left"></i></a>',
+        nextArrow: '<a class="slick-next slick-arrow"><i class="fa fa-angle-right"></i></a>',
+        adaptiveHeight: true
+    })
+};
+
+$(document).ready(function(){
+    app.init();
+})
