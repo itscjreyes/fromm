9cb838cb7c58912c573537c72e5748c6d93214e0
CJ Reyes <cjreyes@Christopher-Reyers-PRO-C02R2B3DG8WN.local>
2019-01-22 14:16:46 -0500

    Navigation for mobile complete

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,48 @@
 const app = {};
 
 app.init = function(){
+    app.navigation();
     app.slider();
+};
+
+app.navigation = function(){
+    $('header nav li.hasChildren').each(function(i){
+        $(this).append('<span><i class="fa fa-angle-down"></i></span>');
+    });
+
+    // Mobile Navigation
+    $('header nav li.hasChildren span').click(function(){
+        var allSubMenus = $('ul.subMenu');
+        var thisSubMenu = $(this).parent().find('ul.subMenu');
+
+        if (thisSubMenu.hasClass('open')){
+            thisSubMenu.removeClass('open');
+            thisSubMenu.slideUp(400);
+        } else {
+            allSubMenus.slideUp(400);
+            allSubMenus.removeClass('open');
+            thisSubMenu.addClass('open');
+            thisSubMenu.slideDown(400);
+        }
+    });
+    
+    $('.mobileToggle').click(function(){
+        $(this).toggleClass('open');
+        $('header nav').toggleClass('open');
+    })
+
+    $(window).on('resize', function(){
+        var mobileNav = $('header nav');
+        if ($(window).width() < 1095) {
+            $('header').addClass('mobile');
+            $('.topBar').appendTo(mobileNav);
+            $('.topPhone').appendTo(mobileNav);
+        } else {
+            $('header').removeClass('mobile');
+            $('.topBar').prependTo('header');
+            $('.topPhone').appendTo('.topLogo .container');
+        }
+    }).resize();
 };
 
 app.slider = function(){
